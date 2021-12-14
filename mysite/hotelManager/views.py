623d092269dd7c41e67e395844f39999d75579f5
david <david.rillh1@gmail.com>
2021-12-14 09:29:01 +0100

    I understood that you need to add variables to the 'context' dictionnary to call them on the template.

--- mysite/hotelManager/views.py
+++ mysite/hotelManager/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import City, Hotel
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the hotelManager index.")
+    list_cities = City.objects.all()
 
-def new(request):
-    return HttpResponse("this is another webpage")
+    context = {"list_cities": list_cities}
+    return render(request, 'hotelManager/index.html', context)
 
+
+def city_details(request, name):
+    list_hotels = Hotel.objects.filter(city__name=name)
+    nb_hotels = len(list_hotels)
+    context = {"list_hotels": list_hotels, "name": name, "nb_hotels": nb_hotels}
+
+    return render(request, 'hotelManager/city_details.html', context)
