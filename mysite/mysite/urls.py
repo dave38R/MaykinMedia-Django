8dc6ebddc1f143f88578a3efb4772764487fc35b
david <david.rillh1@gmail.com>
2021-12-14 09:28:12 +0100

    I realized all we need is two urls, the rest is in hotelManager/urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('hotelManager/', include('hotelManager.urls')),
     path('admin/', admin.site.urls),
+    path('', include('hotelManager.urls')),
 ]
