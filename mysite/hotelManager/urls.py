6fcaab3c94f5fc22b314f3608c95851109b039e7
david <david.rillh1@gmail.com>
2021-12-15 22:14:29 +0100

    I added the urls to the templates

--- mysite/hotelManager/urls.py
+++ mysite/hotelManager/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('city/<str:name>/', views.city_details, name='city_details'),
+    path('add_hotel_in_<str:name>/', views.add_hotel, name="add_hotel"),
+    path('add_city/', views.add_city, name="add_city"),
+
 ]
 
 
